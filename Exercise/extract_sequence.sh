4625f930eecbf2fbe8f9d8d43e6ade25a549e35f
John Oketch <johnoketch@Johns-MacBook-Air.local>
2019-02-26 09:39:46 +0300

    new add

---
+++ Exercise/extract_sequence.sh
@@ -0,0 +1,18 @@
+cd Introduction_To_Linux/
+  186  mkdir Exercise
+  187  cd Exercise/
+  188  mkdir Sequences-unlgn Sequences-algn Sequence-headers
+  189  ls
+  190  cd Sequences-unlgn/
+  191  wget "https://raw.githubusercontent.com/kipkurui/IntroductoryLinux/master/Data/nrf1_seq.fa"
+  192  wget https://raw.githubusercontent.com/kipkurui/IntroductoryLinux/master/Data/nrf1_seq.fa
+  193  wget https://raw.githubusercontent.com/kipkurui/IntroductoryLinux/master/Data/nrf1_seq.fa
+  194  Wget https://raw.githubusercontent.com/kipkurui/IntroductoryLinux/master/Data/nrf1_seq.fa
+  195  wget https://raw.githubusercontent.com/kipkurui/IntroductoryLinux/master/Data/nrf1_seq.fa
+  196  curl https://raw.githubusercontent.com/kipkurui/IntroductoryLinux/master/Data/nrf1_seq.fa > nrf1.fa
+  197  ls
+  198  grep ">" nrf1.fa > ../Sequence-headers/
+  199  grep ">" nrf1.fa > ../Sequence-headers/sequence_names.txt
+  200  cd ../Sequence-headers/
+  201  ls
+  202  less sequence_names.txt 
